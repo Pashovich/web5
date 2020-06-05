313d1b4ff7f3e2b10e67cacc4af2b525aba08f8c
Pashovich <addons125@gmail.com>
2020-06-05 18:15:33 +0700

    hot fix

---
+++ WebTasks/DbMigration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTasks.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebTasks
+{
+    public static class DbMigration
+    {
+        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+
+            return webHost;
+        }
+    }
+}
---
+++ WebTasks/Models/ViewModels/HospitalLabCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTasks.Models.ViewModels
+{
+    public class HospitalLabCreateModel
+    {
+        public Int32 LabId { get; set; }
+    }
+}
